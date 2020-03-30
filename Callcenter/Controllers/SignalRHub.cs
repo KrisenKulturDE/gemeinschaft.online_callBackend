b82f8c095d1e32f5042fcb6877c0ced718020f6a
Fabian Spottog <fabian@spottog.de>
2020-03-30 19:14:25 +0200

    add stresstest

--- Callcenter/Controllers/SignalRHub.cs
+++ Callcenter/Controllers/SignalRHub.cs
@@ -10,6 +10,7 @@ using System.IO;
 using System.Linq;
 using System.Runtime.Serialization.Json;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Callcenter.Controllers
@@ -19,11 +20,22 @@ namespace Callcenter.Controllers
         private readonly Database database;
         private readonly SignInManager<ApplicationUser> siginmanager;
         private readonly UserManager<ApplicationUser> userManager;
+        private static long ConnectionCount;
         public SignalRHub(Database database, SignInManager<ApplicationUser> siginmanager, UserManager<ApplicationUser> userManager)
         {
             this.database = database;
             this.siginmanager = siginmanager;
             this.userManager = userManager;
+        }
+        public override Task OnConnectedAsync()
+        {
+            Interlocked.Increment(ref ConnectionCount);
+            return base.OnConnectedAsync();
+        }
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            Interlocked.Decrement(ref ConnectionCount);
+            return base.OnDisconnectedAsync(exception);
         }
         internal static EntryRequest ParseRequest(string request)
         {
@@ -37,6 +49,22 @@ namespace Callcenter.Controllers
                     return er;
             }
             throw new FormatException($"kann \"{request}\" nicht nach EntryRequest umwandeln");
+        }
+        public async Task scaletest(string eingang)
+        {
+            await Clients.All.SendAsync("scalerespone", new ScaleResponse(eingang ,ConnectionCount));
+        }
+    }
+    public class ScaleResponse
+    {
+        public DateTime timestamp { get; set; }
+        public string response { get; set; }
+        public long countconnections { get; set; }
+        public ScaleResponse(string Response, long Count)
+        {
+            this.timestamp = DateTime.Now;
+            this.response = Response;
+            this.countconnections = Count;
         }
     }
 }
